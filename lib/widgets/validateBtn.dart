36f88921b2e5624de6be15b6a9458889bb2a3420
pratik037 <singhalpratik037@gmail.com>
2019-11-10 00:02:54 +0530

    Handled the errors

--- lib/widgets/validateBtn.dart
+++ lib/widgets/validateBtn.dart
@@ -18,73 +18,86 @@ class ValidateBtn extends StatelessWidget {
           services.dismissKeyBoard(context);
           print("Regex Received: " + regex.text);
           print("Test Data Received: " + test.text);
-          bool result = services.testData(regex.text, test.text);
-          if (result) {
-            showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                      title: Center(child: Text("Evaluation Result")),
-                      actions: <Widget>[
-                        FlatButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text("OK"),
-                        )
-                      ],
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      content: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Icon(
-                            FontAwesomeIcons.checkCircle,
-                            color: Colors.green,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text("Expression is correct"),
+          if (regex.text.isNotEmpty && test.text.isNotEmpty) {
+            bool result = services.testData(regex.text, test.text);
+            if (result) {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        title: Center(child: Text("Evaluation Result")),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text("OK"),
+                          )
                         ],
-                      ),
-                    ));
-          } else {
-            showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                      title: Center(child: Text("Evaluation Result")),
-                      actions: <Widget>[
-                        FlatButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text("OK"),
-                        )
-                      ],
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      content: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Icon(
-                            FontAwesomeIcons.times,
-                            color: Colors.red[700],
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text("Incorrect Expression"),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        content: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.checkCircle,
+                              color: Colors.green,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Expression is correct"),
+                          ],
+                        ),
+                      ));
+            } else {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        title: Center(child: Text("Evaluation Result")),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text("OK"),
+                          )
                         ],
-                      ),
-                    ));
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        content: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.times,
+                              color: Colors.red[700],
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Incorrect Expression"),
+                          ],
+                        ),
+                      ));
+            }
+          }
+          else{
+            Scaffold.of(context).showSnackBar(SnackBar(
+              backgroundColor: Colors.red,
+
+              content: Text("Cannot have empty field", style: TextStyle(fontSize: 25),),
+            ));
           }
         },
         elevation: 3,
         color: Theme.of(context).primaryColor,
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Text("Validate Regex", style: TextStyle(color: Colors.white, fontSize: 20,)),
+          child: Text("Validate Regex",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              )),
         ),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       ),
